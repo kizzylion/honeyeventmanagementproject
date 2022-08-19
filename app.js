f307d37e751075599423cca4d41503074e1e73fc
kizzylion <kztchm@gmail.com>
2022-08-19 22:52:35 +0100

    home image fixture in progress

--- app.js
+++ app.js
@@ -16,3 +16,22 @@ inputs.forEach(input => {
     input.addEventListener("focus", focusFunc);
     input.addEventListener("blur", blurFunc);
 })
+
+
+function sendEmail(){
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "kztchm@gmail.com",
+        Password : "083FB261E8C1AAE0CFDD882B7CEE0D7C4751",
+        To : 'chmkzt.ck@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "New Contact form Enquiry",
+        Body : "Name: " + document.getElementById("name").value
+                + "<br> Email: " + document.getElementById("email").value
+                + "<br> Phone no: " + document.getElementById("phone").value
+                + "<br> Message: " + document.getElementById("message").value
+    }).then(
+      message => alert("Message Sent Successfully")
+    );
+
+}
